541be136ccd5cf1aa50cdac11c7d897ca4867d6b
Eva <evapirozhok@gmail.com>
2023-10-24 13:26:04 +0300

    add hash function; add mask for password

--- Utilts/Utilts.cs
+++ Utilts/Utilts.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace InformBez.Utilts
 {
@@ -23,5 +25,30 @@ namespace InformBez.Utilts
 
             return proc.StandardOutput.ReadToEnd().Replace("UUID", string.Empty).Trim().ToUpper();
         }
+        static byte[] GenerateSalt()
+        {
+            const int SaltLength = 64;
+            byte[] salt = new byte[SaltLength];
+
+            var Rand = RandomNumberGenerator.Create();
+            Rand.GetBytes(salt);
+
+            return salt;
+        }
+        public static byte[] GenerateHash(string password, byte[] salt)
+        {
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            byte[] saltedPassword = new byte[salt.Length + passwordBytes.Length];
+
+            return MD5.HashData(saltedPassword);
+        }
+        public static string GetHashPassword(string password)
+        {
+            byte[] salt = GenerateSalt();
+            byte[] Hash = GenerateHash(password, salt);
+            string HashString = Convert.ToBase64String(Hash);
+            return HashString;
+
+        }
     }
-}
+    }
